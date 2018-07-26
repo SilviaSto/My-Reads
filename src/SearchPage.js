7c89382ae5e1a7223c3cdd235593723dafcee406
Silvia <stoianova.silvia@yahoo.com>
2018-07-26 19:59:55 +0300

    delete state

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -8,7 +8,7 @@ class SearchPage extends Component {
         return(
           <div className="search-books">
             <div className="search-books-bar">
-              <Link to='/' className="close-search" onClick={() => this.setState({ showSearchPage: false })}>
+              <Link to='/' className="close-search">
               Close
               </Link>
               <div className="search-books-input-wrapper">
@@ -20,12 +20,18 @@ class SearchPage extends Component {
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
-                <input type="text" placeholder="Search by title or author"/>
+                <input
+                type="text"
+                placeholder="Search by title or author"
+                value = {this.state.searchedText}
+                onChange={(event)=> event.target.value}
+                />
 
               </div>
             </div>
             <div className="search-books-results">
-              <ol className="books-grid"></ol>
+              <ol className="books-grid">
+              </ol>
             </div>
         </div>
         );
